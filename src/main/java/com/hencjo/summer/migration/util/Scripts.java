50f4b0b2dd7e2d14fc78b049fba2121ee4c7d6ab
Henrik Johansson <hencjo@gmail.com>
2013-01-11 17:34:19 +0100

    Fixed so that SQL scripts can be emdedded in JARs, and reading them in  a nicer way.

--- src/main/java/com/hencjo/summer/migration/util/Scripts.java
+++ src/main/java/com/hencjo/summer/migration/util/Scripts.java
@@ -1,16 +1,24 @@
 package com.hencjo.summer.migration.util;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class Scripts {
 	public static void executeScript(Connection connection, String filename) throws IOException, SQLException {
-		String readLines = Resources.toString(Resources.getResource(filename), Charsets.UTF8);
-		for (String sql : readLines.split(";")) {
-			try (Statement s = connection.createStatement()) {
-				s.executeUpdate(sql);
+  	URL resource = Resources.getResource(filename);
+		try (InputStream openStream = resource.openStream();
+				Scanner scanner2 = new Scanner(openStream, Charsets.UTF8.name());
+				Scanner scanner = scanner2.useDelimiter(";")) {
+			while (scanner.hasNext()) {
+				String sql = scanner.next();
+				try (Statement s = connection.createStatement()) {
+					s.executeUpdate(sql);
+				}
 			}
 		}
 	}
