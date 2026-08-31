701f7f8eb0478ad62e1d4b3fafcfde56e1f65986
Henrik Sjöstrand <henrik@sjostrand.at>
2026-08-31 18:46:05 +0200

    Move duration report to after commit.

--- src/main/java/com/hencjo/summer/migration/Migrator.java
+++ src/main/java/com/hencjo/summer/migration/Migrator.java
@@ -13,20 +13,20 @@ import com.hencjo.summer.migration.dsl.MigrationsDescription;
 public final class Migrator {
 	private final SchemaMigrations schemaMigrations;
 	private final Database database = new Database();
-	
+
 	public Migrator() {
 		this.schemaMigrations = new SchemaMigrations("schema_migrations");
 	}
-	
+
 	public void migrate(Connection connection, MigrationsDescription upgradeDescription) throws IOException, SQLException {
 		connection.setAutoCommit(false);
-		
+
 		if (!database.containsTables(connection))
 			schemaMigrations.create(connection);
-			
-		if (!schemaMigrations.exists(connection)) 
+
+		if (!schemaMigrations.exists(connection))
 			throw new RuntimeException("Expected " + schemaMigrations.tableName + " to exist and contain the version number. It doesn't.");
-		
+
 		migrations(connection, upgradeDescription.migrations);
 	}
 
@@ -36,10 +36,10 @@ public final class Migrator {
 			System.out.println("Applying migration \"" + migration.key + "\" ... ");
 			Instant start = Instant.now();
 			for (UpgradeStep upgradeStep : migration.upgradeSteps) upgradeStep.apply(connection);
-			Duration duration = Duration.between(start, Instant.now());
-			System.out.printf("Migration \"%s\" completed in %d.%03ds%n", migration.key, duration.getSeconds(), duration.getNano() / 1_000_000);
 			schemaMigrations.addApplied(connection, migration.key);
 			connection.commit();
+			Duration duration = Duration.between(start, Instant.now());
+			System.out.printf("Migration \"%s\" completed in %d.%03ds%n", migration.key, duration.getSeconds(), duration.getNano() / 1_000_000);
 		}
 	}
 }
